94dc84a72ccbf3f70729bb0f718d86a5d2b0de22
seeday <seeday@users.noreply.github.com>
2021-12-09 16:21:26 -0600

    Build for many architectures, hopefully

--- flake.nix
+++ flake.nix
@@ -4,33 +4,36 @@
   inputs = {
     pulsar.url = "github:apache/pulsar/v2.8.1";
     pulsar.flake = false;
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, flake-utils, pulsar }: {
-    defaultPackage.x86_64-linux =
-      let pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in pkgs.stdenv.mkDerivation {
-        name = "libpulsar";
-        src = self.inputs.pulsar;
+  outputs = { self, nixpkgs, flake-utils, pulsar }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          libpulsar = pkgs.stdenv.mkDerivation {
+            name = "libpulsar";
+            src = self.inputs.pulsar;
 
-        # it is of course, undocumented that you need to lead with source/
-        sourceRoot = "source/pulsar-client-cpp";
+            # it is of course, undocumented that you need to lead with source/
+            sourceRoot = "source/pulsar-client-cpp";
 
-        nativeBuildInputs = with pkgs; [
-          cmake
-          openssl
-          zlib
-          curlMinimal
-          boost
-          python3
-          python3Packages.boost
-          protobuf
-          zstd
-          snappy
-          gtest
-        ];
-      };
-  };
+            nativeBuildInputs = with pkgs; [
+              cmake
+              openssl
+              zlib
+              curlMinimal
+              boost
+              python3
+              python3Packages.boost
+              protobuf
+              zstd
+              snappy
+              gtest
+            ];
+          };
+        };
+        defaultPackage = packages.libpulsar;
+      });
 }
